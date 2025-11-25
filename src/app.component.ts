4ded432abb4e51dd3afdc4fb3107bf598052b370
jywu3 <92764750+lolisaigao1234@users.noreply.github.com>
2025-11-25 00:14:29 -0600

    Add new components to app imports
    Imported Header, Hero, Projects, Contact, and Footer components into the app component. Also added an empty app.component.css file for future styling.

--- src/app.component.ts
+++ src/app.component.ts
@@ -2,10 +2,24 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OpeningScreenComponent } from './components/opening-screen/opening-screen.component';
 
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, OpeningScreenComponent],
+  imports: [
+    RouterOutlet,
+    OpeningScreenComponent,
+    HeaderComponent,
+    HeroComponent,
+    ProjectsComponent,
+    ContactComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
